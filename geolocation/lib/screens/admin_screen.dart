89fb0a2fc11cc1b21f77a921665a3d320cddf8e3
meenansai <v.saimeenan@gmail.com>
2020-06-04 13:56:42 +0530

    view users on map merged position changed

--- geolocation/lib/screens/admin_screen.dart
+++ geolocation/lib/screens/admin_screen.dart
@@ -33,84 +33,90 @@ class _AdminScreenState extends State<AdminScreen> {
   Widget build(BuildContext context) {
     var usersList = Provider.of<UserProvider>(context, listen: false);
     return _isLoading
-          ? Scaffold(
-            appBar: AppBar(title: Text('Welcome'),),
-            body: Center(child: CircularProgressIndicator()) ,
+        ? Scaffold(
+            appBar: AppBar(
+              title: Text('Welcome'),
+            ),
+            body: Center(child: CircularProgressIndicator()),
           )
-         
-          : Scaffold(
-      drawer:AppBarMenu(),
-      appBar: AppBar(
-        title: Text('Users'),
-        actions: [
-          FlatButton(
-            onPressed: () {
-              Navigator.pushReplacementNamed(context,'/');
-            },
-            child:Row(
-              children: [
-                Icon(Icons.refresh,color: Colors.white,),
-                SizedBox(
-                  width: 5,
-                ),
-                Text('Refresh',style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15
-                ),),
+        : Scaffold(
+            drawer: AppBarMenu(),
+            appBar: AppBar(
+              title: Text('Users'),
+              actions: [
+                FlatButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/');
+                    },
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.refresh,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          'Refresh',
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      ],
+                    )
+                    // Icon(Icons.refresh,color: Colors.wh,),
+                    ),
               ],
-            ) 
-            // Icon(Icons.refresh,color: Colors.wh,),
-          ),
-        ],
-      ),
-      body: 
-          Column(
-                      children:[ 
-                        Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: RaisedButton(
-                          textColor: Colors.white,
-                          color: Colors.blueGrey,
-                        child: Text(" View users on map",style: TextStyle(fontSize: 16),),
-                        onPressed: () {
-                          Navigator.of(context).pushNamed(
-                            AdminMapScreen.routeName,
-                           ); },
-                           shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(30.0),
-                          ),
-                          ),
-                      ),
-                        Flexible( flex: 1,
-                                              child: Container(
-                         
+            ),
+            body: Column(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Container(
                     padding: EdgeInsets.all(10),
                     child: ListView.builder(
-                        itemBuilder: (ctx, index) {
-                          return UserListItem(
-                            userId: usersList.users[index].id,
-                            userName: usersList.users[index].name,
-                            latitude: usersList.users[index].latitude,
-                            longitude: usersList.users[index].longitude,
-                            designation: usersList.users[index].designation,
-                          );
-                        },
-                        itemCount: usersList.users.length,
+                      itemBuilder: (ctx, index) {
+                        return UserListItem(
+                          userId: usersList.users[index].id,
+                          userName: usersList.users[index].name,
+                          latitude: usersList.users[index].latitude,
+                          longitude: usersList.users[index].longitude,
+                          designation: usersList.users[index].designation,
+                        );
+                      },
+                      itemCount: usersList.users.length,
                     ),
                   ),
-                      ),
-                      
-                      ],),
-       
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Colors.blueGrey,
+                    child: Text(
+                      " View users on map",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(
+                        AdminMapScreen.routeName,
+                      );
+                    },
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
 
-      // floatingActionButton:
-      //     FloatingActionButton.extended(
-      //       onPressed: () {},
-      //       icon: Icon(Icons.person_pin_circle,color: Colors.white,),
-      //       label: Text('Locate'),
-      //     ),
+            // floatingActionButton:
+            //     FloatingActionButton.extended(
+            //       onPressed: () {},
+            //       icon: Icon(Icons.person_pin_circle,color: Colors.white,),
+            //       label: Text('Locate'),
+            //     ),
 
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-    );
+            floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+          );
   }
 }
